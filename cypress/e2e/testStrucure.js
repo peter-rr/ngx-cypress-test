15a10db2736eb5ef4cfe3c968cb693a08d563059
Pedro Ramos <pedroramosromero@gmail.com>
2024-10-28 20:07:04 +0100

    Add dummy file structure file

---
+++ cypress/e2e/testStrucure.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('First test suite', () => {
+
+    describe('suite section', () => {
+
+        beforeEach('login', () => {
+            //repeat for every test
+        })
+
+        it('first test', () => {
+            //put the code of the test
+        })
+    
+        it('second test', () => {
+            //put the code of the test
+        })
+        
+    })
+
+    it('first test', () => {
+        //put the code of the test
+    })
+
+    it('second test', () => {
+        //put the code of the test
+    })
+
+    it('third test', () => {
+        //put the code of the test
+    })
+
+})
