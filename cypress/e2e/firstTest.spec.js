571c847ed54074bce852981b801dd6bc98f57202
Pedro Ramos <pedroramosromero@gmail.com>
2024-10-30 18:47:23 +0100

    Interact with radio buttons and checkboxes

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -88,7 +88,7 @@ describe('First test suite', () => {
         })
     })
 
-    it.only('extract text values', () => {
+    it('extract text values', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -116,12 +116,36 @@ describe('First test suite', () => {
 
         // 5 invoke property -> Get the values out of an input field. Those values are hidden inside of the properties
         cy.get('#exampleInputEmail1').type('test@test.com')
-        // cy.get('#exampleInputEmail1').should('contain', 'test@test.com') -> FAILS since the text entered is not an HTML text
+        //cy.get('#exampleInputEmail1').should('contain', 'test@test.com') -> FAILS since the text entered is not an HTML text
         cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com') // -> The text entered is an HTML property ('value' in this case)
         cy.get('#exampleInputEmail1').invoke('prop', 'value').then( property => {
             expect(property).to.equal('test@test.com')
         })
 
+    })
+
+    it('radio buttons', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons).eq(0).check({force: true}).should('be.checked')
+            cy.wrap(radioButtons).eq(1).check({force: true})
+            cy.wrap(radioButtons).eq(0).should('not.be.checked')
+            cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+    })
+
+    it('checkboxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').check({force: true})
+        //cy.get('[type="checkbox"]').uncheck({force: true})
+        cy.get('[type="checkbox"]').eq(0).click({force: true})
+        cy.get('[type="checkbox"]').eq(1).check({force: true})
     })
 
 })
